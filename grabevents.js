9b970310ff4510945b9f504da17014fe337ddefc
LoganBates <loganbates@gmail.com>
2014-09-15 18:46:56 -0600

    fast code done?

--- grabevents.js
+++ grabevents.js
@@ -22,10 +22,19 @@ for (var i=0; i<input.length; ++i) {
         flattened.push(current[j]);
 }
 */
-function flatten_slow(input){	
+function flatten_slow(input){
+/*	
     return input.reduce(function(a, b) {
     	return a.concat(b);
 	}, []);
+*/
+var flattened=[];
+	for (var i=0; i<input.length; ++i) {
+	    var current = input[i];
+	    for (var j=0; j<current.length; ++j)
+	        flattened.push(current[j]);
+	}
+	return flattened;
 }
  
 async.map([1,2,3,4,5,6,7,8,9,10], getClassEventsForPage, function(err, results){
